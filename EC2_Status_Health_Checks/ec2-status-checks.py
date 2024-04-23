75e1e47c77da3c782ef5cc704dd12107bdf07b53
Rus Rares <rusrares1997@gmail.com>
2024-04-23 12:05:39 +0300

    added scheduler that triggers the program automatically

--- EC2_Status_Health_Checks/ec2-status-checks.py
+++ EC2_Status_Health_Checks/ec2-status-checks.py
@@ -1,13 +1,21 @@
 import boto3
+import schedule
 
 ec2_client = boto3.client('ec2')
 ec2_resource = boto3.resource('ec2')
 
+# Function to known which instances are in which state
+def check_instance_status():
+    statuses = ec2_client.describe_instance_status()
+    for status in statuses ['InstanceStatuses']:
+        ins_status = status['InstanceStatus']['Status']
+        sys_status = status['SystemStatus']['Status']
+        state = status['InstanceState']['Name']
+        print(f"Instace {status['InstanceId']} is {state} with instance status {ins_status} and system status {sys_status}")
 
 
-statuses = ec2_client.describe_instance_status()
-for status in statuses ['InstanceStatuses']:
-    ins_status = status['InstanceStatus']['Status']
-    sys_status = status['SystemStatus']['Status']
-    state = status['InstanceState']['Name']
-    print(f"Instace {status['InstanceId']} is {state} with instance status {ins_status} and system status {sys_status}")
+# Scheduler that triggers the program automatically every 5 minutes
+schedule.every(5).minutes.do(check_instance_status)
+
+while True:
+    schedule.run_pending()

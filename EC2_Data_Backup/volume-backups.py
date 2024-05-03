fc5d0151d1497314f879951a01914ded37adb209
Rus Rares <rusrares1997@gmail.com>
2024-05-03 21:39:31 +0300

    implemented scheduler that will do snapshots every day

--- EC2_Data_Backup/volume-backups.py
+++ EC2_Data_Backup/volume-backups.py
@@ -2,14 +2,20 @@
 # and creates Snapshots for those volumes.
 
 import boto3
+import schedule
 
 ec2_client = boto3.client('ec2', region_name='eu-west-3')
 
-volumes = ec2_client.describe_volumes()
-for volume in volumes['Volumes']:
-    new_snapshot = ec2_client.create_snapshot(
-        VolumeId=volume['VolumeId']
-    )
-    print(new_snapshot)
+def create_voume_snapshots():
+    volumes = ec2_client.describe_volumes()
+    for volume in volumes['Volumes']:
+        new_snapshot = ec2_client.create_snapshot(
+            VolumeId=volume['VolumeId']
+        )
+        print(new_snapshot)
 
 
+schedule.every().day.do(create_voume_snapshots)
+
+while True:
+    schedule.run_pending()

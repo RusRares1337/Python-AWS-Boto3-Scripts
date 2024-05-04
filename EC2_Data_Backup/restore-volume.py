6cfef1a720bb2c9fde1739dc1944dc49998499aa
Rus Rares <rusrares1997@gmail.com>
2024-05-05 02:38:37 +0300

    script that will restore volume and attach a new volume to ec2 instance

--- EC2_Data_Backup/restore-volume.py
+++ EC2_Data_Backup/restore-volume.py
@@ -2,13 +2,13 @@
 # and attach a new volume to EC2 Instance
 
 import boto3
+from operator import itemgetter
 
 ec2_client = boto3.client('ec2', region_name="eu-west-3")
 ec2_resource = boto3.resource('ec2', region_name="eu-west-3")
 
-instance_id = 'i-0bc53c17e7a0c29b3'
+instance_id = "i-0bc53c17e7a0c29b3"
 
-# Filter the volumes that belong to that specific instance id
 volumes = ec2_client.describe_volumes(
     Filters=[
         {
@@ -19,4 +19,42 @@ volumes = ec2_client.describe_volumes(
 )
 
 instance_volume = volumes['Volumes'][0]
-print(instance_volume)
+
+snapshots = ec2_client.describe_volumes(
+    OwnerIds=['self'],
+    Filters=[
+        {
+            'Name': 'volume-id',
+            'Values': [instance_volume['VolumeId']]
+        }
+    ]
+)
+
+latest_snapshot = sorted(snapshots['Snapshots'], key=itemgetter('StartTime'), reverse=True)[0]
+
+new_volume = ec2_client.create_volume(
+    SnapshotId=latest_snapshot['SnapshotId'],
+    AvailabilityZone="eu-west-3b",
+    TagSpecifications=[
+        {
+            'ResourceType': 'volume',
+            'Tags': [
+                {
+                    'Key': 'Name',
+                    'Value': 'Prod'
+                },
+            ]
+        },
+    ]
+)
+
+# Check the state of the volume until it becomes available and attach new volume to the instance
+while True:
+    ec2_resource.Volume(new_volume['VolumeId'])
+    print(vol.state)
+    if vol.state == 'available':
+        ec2_resource.Instance(instance_id).attach_volume(
+            VolumeId=new_volume['VolumeId'],
+            Device='/dev/xvdb'
+        )
+        break

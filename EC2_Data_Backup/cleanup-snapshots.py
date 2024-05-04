8b9bcda4dd30fe0012a9bdf566f6ce55e9945aae
Rus Rares <rusrares1997@gmail.com>
2024-05-05 00:04:06 +0300

    updated script

--- EC2_Data_Backup/cleanup-snapshots.py
+++ EC2_Data_Backup/cleanup-snapshots.py
@@ -6,17 +6,39 @@ from operator import itemgetter
 
 ec2_client = boto3.client('ec2', region_name="eu-west-3")
 
-# Fetch our snapshots
-snapshots = ec2_client.describe_snapshots(
-    OwnerIds=['self']
+# List all the volumes with tag name:Environment and value:Prod
+volumes = ec2_client.describe_volumes(
+    Filters=[
+        {
+            'Name': 'tag:Environment',
+            'Values': ['Prod']
+        }
+    ]
 )
 
-# Sort the snapshot lists by StartTime, with reverse=True we sort them in DESC order
-sorted_by_date = sorted(snapshots['Snapshots'], key=itemgetter('StartTime'), reverse=True)
-
-# Delete all snapshots starting from 3rd item
-for snap in sorted_by_date[2:]:
-    ec2_client.delete_snapshot(
-        SnapShotId=snap['Snapshot']
+for volume in volumes['Volumes']:
+    # Fetch our snapshots
+    snapshots = ec2_client.describe_snapshots(
+        OwnerIds=['self'],
+        Filters=[
+        {
+            'Name': 'volume-id',
+            'Values': [volume[VolumeId]]
+        }
+    ]
     )
-    
+
+    # Sort the snapshot lists by StartTime, with reverse=True we sort them in DESC order
+    sorted_by_date = sorted(snapshots['Snapshots'], key=itemgetter('StartTime'), reverse=True)
+
+    # Delete all snapshots starting from 3rd item
+    for snap in sorted_by_date[2:]:
+        ec2_client.delete_snapshot(
+            SnapShotId=snap['Snapshot']
+        )
+
+    # Fetch our snapshots
+    snapshots = ec2_client.describe_snapshots(
+        OwnerIds=['self']
+    )
+

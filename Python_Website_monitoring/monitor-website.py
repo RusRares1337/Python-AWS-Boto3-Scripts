934a9dbcb3352c291f377b24b87250dcc86ab3ef
Rus Rares <rusrares1997@gmail.com>
2024-05-12 22:03:15 +0300

    configured vars as env vars

--- Python_Website_monitoring/monitor-website.py
+++ Python_Website_monitoring/monitor-website.py
@@ -1,7 +1,20 @@
 import requests
+import smtplib
+import os
+
+EMAIL_ADDRESS = os.environ.get('EMAIL_ADDRESS')
+EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 
 response = requests.get('http://13.38.29.19:8080/')
 if response.status_code == 200:
     print("Application is running successfully")
 else:
     print('Application Down. Fix it!')
+    # send email to me
+    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+        smtp.starttls()
+        smtp.ehlo()
+        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        msg = EMAIL_ADDRESS, EMAIL_PASSWORD, "Subject: SITE DOWN\nFix the issue! Rstart the application."
+        smtp.sendmail(EMAIL_ADDRESS, EMAIL_PASSWORD, msg)
+

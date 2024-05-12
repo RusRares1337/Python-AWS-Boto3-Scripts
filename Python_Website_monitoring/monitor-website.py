781cb033f1853f3915007dda31b014f0a5464bd8
Rus Rares <rusrares1997@gmail.com>
2024-05-12 22:24:47 +0300

    configured send notification function

--- Python_Website_monitoring/monitor-website.py
+++ Python_Website_monitoring/monitor-website.py
@@ -1,3 +1,7 @@
+# Scheduled task to monitor application health
+# Automated Email notification
+# Restart application and reboot server
+
 import requests
 import smtplib
 import os
@@ -5,16 +9,24 @@ import os
 EMAIL_ADDRESS = os.environ.get('EMAIL_ADDRESS')
 EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 
-response = requests.get('http://13.38.29.19:8080/')
-if response.status_code == 200:
-    print("Application is running successfully")
-else:
-    print('Application Down. Fix it!')
-    # send email to me
+def send_notification(email_msg):
     with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
         smtp.starttls()
         smtp.ehlo()
         smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-        msg = EMAIL_ADDRESS, EMAIL_PASSWORD, "Subject: SITE DOWN\nFix the issue! Rstart the application."
-        smtp.sendmail(EMAIL_ADDRESS, EMAIL_PASSWORD, msg)
+        message = EMAIL_ADDRESS, EMAIL_PASSWORD, f"Subject: SITE DOWN\n{email_msg}"
+        smtp.sendmail(EMAIL_ADDRESS, EMAIL_PASSWORD, message)
 
+
+try:
+    response = requests.get('http://15.237.181.14:8080/')
+    if response.status_code == 200:
+        print("Application is running successfully")
+    else:
+        print('Application Down. Fix it!')
+        msg = f'Application returned {response.status_code}'
+        send_notification(msg)
+except Exception as ex:
+    print(f'Connection error happened: {ex}')
+    msg = 'Application not accesible at all.'
+    send_notification(msg)
